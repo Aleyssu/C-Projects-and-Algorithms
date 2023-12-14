f65310ecae229bd7e6d245a80ab295ef176873cf
DailySugar <alexsunn30@gmail.com>
2023-12-14 16:56:17 -0500

    Added start of advent_4

---
+++ advent_of_code_2023/advent_4.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Solutions for the 2023 Advent of Code day 4
+ * 
+ * https://adventofcode.com/2023/day/4
+*/
+
+// Returns true if char is a number; false otherwise.
+bool is_num(char c) {
+    return c >= '0' && c <= '9';
+}
+
+/*
+ * Returns the value of the next integer in the current line of the stream, and 0 if 
+ * not found within the line or before a '|' character. 
+ * Stream position is set to the end said integer, and last read character is stored in c.
+*/
+int get_num(ifstream &f, char &c) {
+    int curr = f.get(), num = 0;
+    while (!is_num(curr)) {
+        if (curr == '\n' || curr == EOF || curr == '|') {
+            c = curr;
+            return 0;
+        }
+        curr = f.get();
+    }
+    do {
+        num = num * 10 + curr - '0';
+        curr = f.get();
+    } while (is_num(curr));
+
+    c = curr;
+    return num;
+}
+
+// Part one of the day's solutions - assumes that every line is the same length.
+int part_1(ifstream &f) {
+    f.clear();
+    f.seekg(0);
+    int sum = 0, num, points;
+    char c = 1;
+    string s;
+    unordered_set<int> winning_nums;
+    
+    while (c && c != EOF) {
+        for (c = f.get(); c != ':'; c = f.get());
+        num =  get_num(f, c);
+        while (num && c != '|') {
+            winning_nums.insert(num);
+            num = get_num(f, c);
+        } 
+        num = get_num(f, c);
+        points = 1;
+        while (num) {
+            if (winning_nums.find(num) != winning_nums.end()) {
+                points *= 2;
+                // cout << num << ' ';
+            }
+            if (c == '\n' || c == EOF) break;
+            num = get_num(f, c);
+        } 
+        // cout << '\n';
+        winning_nums.clear();
+        sum += points / 2;
+    }
+    return sum;
+}
+
+// Part two of the day's solutions - assumes that every line is the same length.
+int part_2(ifstream &f) {
+    return 0;
+}
+
+
+int main() {
+    ifstream f("advent_4.txt");    
+
+    cout << "Part 1: " << part_1(f) << '\n';
+    // cout << "Part 2: " << part_2(f) << '\n';
+}
